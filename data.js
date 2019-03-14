4ca0740bb318f139c8037ee749dd3ee32d8baccc
Ghabriel Villarreal <ghabriel.villarreal@tuten.cl>
2019-03-14 11:09:22 -0400

    File System

--- data.js
+++ data.js
@@ -1,5 +1,16 @@
 const { student, getAverage } = require('./requireIntern');
+const fs = require('fs');
 
 let { name, age, notes: { mathematics, english, programming }} = student;
 
 console.log("The average of student " + name + " is " + getAverage(mathematics, english, programming));
+
+let createFile = (student) => {
+	text = "The average of student " + name + " is " + getAverage(mathematics, english, programming) + "\n";
+	fs.writeFile('average.txt', text, (err) => {
+		if (err) throw (err);
+		console.log('File created');
+	})
+}
+
+createFile(student);
